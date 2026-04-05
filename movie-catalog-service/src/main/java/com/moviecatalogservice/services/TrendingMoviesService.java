104a2a282fd31c39d78263de8ab1ed1b5e2e05ca
yomna <yomnay888@gmail.com>
2026-04-05 20:39:49 +0200

    improved code readability.

--- movie-catalog-service/src/main/java/com/moviecatalogservice/services/TrendingMoviesService.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/services/TrendingMoviesService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TrendingMoviesService {
 
-    // "trending-movies-service" must exactly match the key in application.properties:
-    // grpc.client.trending-movies-service.address=...
     @GrpcClient("trending-movies-service")
     private TrendingMoviesServiceGrpc.TrendingMoviesServiceBlockingStub stub;
 
